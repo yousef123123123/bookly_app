70cf7d309607a4ecbb1a7f5ae922164ef0889238
John Doe <youssifgamal123@gmail.com>
2024-06-28 06:13:07 +0300

    add feayterd listview

--- lib/Feauters/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Feauters/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,26 +1,23 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class featuredListviewItem extends StatelessWidget {
+  const featuredListviewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              color: Colors.red,
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(
-                  AssetsData.testImages,
-                ),
-              )),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                AssetsData.testImages,
+              ),
+            )),
       ),
     );
   }
--- lib/Feauters/home/presentation/views/widgets/home_view_body.dart
+++ lib/Feauters/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/Feauters/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Feauters/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -8,7 +9,28 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [CustomAppBar(), CustomListViewItem()],
+      children: const [
+        CustomAppBar(),
+        FeauterdBooksListView(),
+      ],
+    );
+  }
+}
+
+class FeauterdBooksListView extends StatelessWidget {
+  const FeauterdBooksListView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: featuredListviewItem(),
+            );
+          }),
     );
   }
 }
