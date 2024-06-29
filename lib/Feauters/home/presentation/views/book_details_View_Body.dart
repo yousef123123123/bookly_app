756f02fdd623b63cc743b7ea614f89f5ec526d6e
John Doe <youssifgamal123@gmail.com>
2024-06-29 09:32:34 +0300

    add custom scroll View

--- lib/Feauters/home/presentation/views/book_details_View_Body.dart
+++ lib/Feauters/home/presentation/views/book_details_View_Body.dart
@@ -14,58 +14,69 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          customAppBarBookDetails(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          Text('The Jungle Book',
-              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold)),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .8,
-            child: Text('Rudyard Kipling',
-                style: Styles.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BooksAction(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          SimilarBoxListView(),
-          const SizedBox(
-            height: 10,
-          )
-        ],
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: [
+                  customAppBarBookDetails(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: width * .2),
+                    child: CustomBookImage(),
+                  ),
+                  const SizedBox(
+                    height: 43,
+                  ),
+                  Text('The Jungle Book',
+                      style: Styles.textStyle30
+                          .copyWith(fontWeight: FontWeight.bold)),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Opacity(
+                    opacity: .8,
+                    child: Text('Rudyard Kipling',
+                        style: Styles.textStyle18.copyWith(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(
+                    height: 37,
+                  ),
+                  const BooksAction(),
+                  Expanded(
+                    child: const SizedBox(
+                      height: 50,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'You can also like',
+                      style: Styles.textStyle16
+                          .copyWith(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SimilarBoxListView(),
+                  const SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
+            ))
+      ],
     );
   }
 }
